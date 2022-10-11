6556f0316ea882d5d9622769a0e3783fcc7fc180
Artur <helter@wp.pl>
2022-10-11 15:59:39 +0200

    resolving but not with optimal solution

--- src/components/Products/ShowMoreButton.tsx
+++ src/components/Products/ShowMoreButton.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 interface ShowMoreButtonType {
-    onClickHandler: () => void;
+    onClickHandler: ()=>void;
 }
 
 
-const ShowMoreButton: React.FC<ShowMoreButtonType> = (onClickHandler) => {
+const ShowMoreButton: React.FC<any> = (onClickHandler) => {
   return (
     <button className="text-base font-medium border py-3 px-9 border-black rounded-xl hover:text-red-400 hover:border-red-400" onClick={ onClickHandler }>SHOW MORE</button>
   )
