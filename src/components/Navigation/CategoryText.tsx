44839ddc46ed551d928dcad64ff04e46868d78b8
Artur <helter@wp.pl>
2022-10-17 14:31:08 +0200

    uderline category while active

--- src/components/Navigation/CategoryText.tsx
+++ src/components/Navigation/CategoryText.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 interface CategoryTextType {
     category: string;
@@ -7,16 +7,22 @@ interface CategoryTextType {
 }
 
 const CategoryText: React.FC<CategoryTextType> = ({category, categoryId, onCategoryHandle}) => {
+  const [searchParams] = useSearchParams();
+  const categoryFromURL = searchParams.get('category');
+  const isCategoryActive = categoryFromURL === category.toLowerCase()
+
+  const textRed400 = 'text-red-400'
+
   return (
-    <Link to={`/?category=${category?.toLocaleLowerCase()}`}
+    <Link to={`/?category=${category?.toLowerCase()}`}
     className="cursor-pointer" 
     onClick={onCategoryHandle} 
     id={`${categoryId}`}>
-       <p className='text-sm font-normal whitespace-nowrap hover:text-red-400 text-gray-600 '>{category}</p> 
+       <p className= {`text-sm font-normal whitespace-nowrap hover:${textRed400} ${isCategoryActive ? textRed400 : 'text-gray-600'} `}>{category}</p> 
     </Link>
     
 
   )
 }
 
-export default CategoryText
+export default CategoryText 
