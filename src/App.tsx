17c1fedb2784f65fe488aca42b2c459b6c4e3105
Artur <helter@wp.pl>
2022-10-18 12:18:23 +0200

    resolving letter issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css';
 import FilterArticles from './components/FilterArticles/FilterArticles';
-import Header from './components/Header/Header';
-import CategoryNavigation from './components/Navigation/CategoryNavigation';
-import Navigation from './components/Navigation/Navigation';
-import Products from './components/Products/index'; 
+import Header from './components/header/Header';
+import CategoryNavigation from './components/navigation/CategoryNavigation';
+import Navigation from './components/navigation/Navigation';
+import Products from './components/products/index'; 
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import { Route, Routes } from 'react-router-dom';
 import PageNotFound from './pages/PageNotFound';
