bc00e462d7f3d6d760138ca590d56a16cbddd5f2
Artur <helter@wp.pl>
2022-10-27 18:45:23 +0200

    add score searching

--- src/hooks/use-search-results-menu.tsx
+++ src/hooks/use-search-results-menu.tsx
@@ -3,6 +3,27 @@ import useProducts from './use-products';
 import { useSearchParams } from 'react-router-dom';
 import { ResponseProduct} from './products-sort-by-category';
 
+interface Field {
+  field: string,
+  weight: number
+}
+
+const fields: Field[] = [{
+  field: 'title',
+  weight: 10,
+},
+{
+  field: 'description',
+  weight: 9,
+}]
+
+interface FoundProduct {
+  score: number 
+  product: ResponseProduct
+}
+
+
+
 
 const useSearchResultsMenu= (inputText:string, setInputText:React.Dispatch<React.SetStateAction<string>>) => {
     const {allProducts}= useProducts();
@@ -13,6 +34,20 @@ const useSearchResultsMenu= (inputText:string, setInputText:React.Dispatch<React
     const [isOpen, setIsOpen] = useState(false);
     const [foundProducts, setFoundProducts] = useState<null |ResponseProduct[]>(null)
 
+    const checkString = (product: ResponseProduct, field: Field): number => {
+      const text: string = product[field.field as keyof typeof product] as string 
+
+      if(text?.toLowerCase()?.startsWith(inputText.toLowerCase())){
+        return field.weight + 1
+      } 
+
+      if(text?.toLowerCase()?.includes(inputText.toLowerCase())){
+        return field.weight
+      } 
+
+      return 0
+    }
+
     useEffect(() =>{
         if (inputText === '') {
           setIsOpen(false)
@@ -22,10 +57,25 @@ const useSearchResultsMenu= (inputText:string, setInputText:React.Dispatch<React
             return title?.toLowerCase()?.startsWith(inputText.toLowerCase())
           }
           ))
+
+          let foundProducts: FoundProduct[]  = []
+
+          allProducts.map((product: ResponseProduct) => {
+            let weight = 0
+            fields.map((field: Field) => {
+              if(weight === 0){
+                weight = checkString(product, field)
+                if(weight > 0)
+                  foundProducts.push({score: weight, product: product})
+              }
+            })
+          })
+ 
+          setFoundProducts(foundProducts.sort((a,b) => b.score - a.score).map(fp => fp.product).slice(0, 5))
+         
         }
       },[inputText])
 
-      
     useEffect(()=>{
         if (foundProducts?.length === 0) {
         setIsOpen(false)
