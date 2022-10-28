830f9a3453fa54e0e9e514d20e22cd9f51732c49
Artur <helter@wp.pl>
2022-10-28 12:03:20 +0200

    implement dynamic score for FiveStarScore in Card

--- src/components/Products/Card.tsx
+++ src/components/Products/Card.tsx
@@ -13,8 +13,11 @@ const Card: React.FC<CardData> = ({price, title, imageSource}) => {
 
   const onImageError = (e: React.SyntheticEvent<HTMLImageElement, Event>) =>{
     e.currentTarget.src = noImagePath
-  }      
-
+  }    
+  
+  const dynamicScore = Number(((Math.random()* (5-1) + 1)).toFixed(1))
+  
+ 
   return (
     <div className="w-1/5 pb-8 cursor-pointer">
         <div className='overflow-hidden rounded-xl'>
@@ -23,7 +26,7 @@ const Card: React.FC<CardData> = ({price, title, imageSource}) => {
         
         <p className='pt-4 text-xl font-medium'> {price}</p>
         <div className='w-20 flex gap-x-1 py-2'>
-          <FiveStarScore quantity={5} score={4.6} 
+          <FiveStarScore quantity={5} score={dynamicScore} 
           fillColor="rgb(248 113 113)" stroke="rgb(248 113 113)" emptyColor="rgb(241 245 249)" />
         </div>
         <p className='text-xs'>{title}</p>
