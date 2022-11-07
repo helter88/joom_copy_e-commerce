3a6611a18c5e78ee7b4d48ad662b29cad56648cf
Artur <helter@wp.pl>
2022-11-07 17:24:02 +0100

    add BuyForButton to YourCart

--- src/components/cart/your-cart.tsx
+++ src/components/cart/your-cart.tsx
@@ -1,4 +1,5 @@
 import {useRef, useState} from 'react'
+import BuyForButton from '../ui/buttons/buy-for-button';
 
 const YourCart = () => {
     const [isValid, setIsValid]= useState(true)
@@ -16,13 +17,14 @@ const YourCart = () => {
   return (
     <div className=' my-4 bg-white p-5 rounded-xl'>
       <p className='text-xl font-medium'>Your cart</p>
-      <div className='mt-8 flex gap-x-12'>
+      <div className='mt-8 flex gap-x-10'>
         <input type="text" placeholder='Promo code' className='rounded-xl h-11 outline-none 
         border pl-4 w-2/3' ref={textref} onKeyDown={onHandleKeyDown}/>
         <button className='text-base font-medium text-red-400'
             onClick={onClickHandler}>Apply</button>
       </div>
       {isValid || <p className='ml-4 text-sm my-2 text-red-400'>This is an invalid promo code</p>}
+      <BuyForButton />
       
     </div>
   )
---
+++ src/components/ui/buttons/buy-for-button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BuyForButton = () => {
+  return (
+    <button className='w-full bg-red-400 text-white text-lg 
+    py-3 rounded-xl hover:opacity-80 duration-300 my-5'>
+      Buy for PLN
+    </button>
+  )
+}
+
+export default BuyForButton
