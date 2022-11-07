8ab1634cc62e66f7e0b66d13d45eb766669165b3
Artur <helter@wp.pl>
2022-11-07 17:09:34 +0100

    add input validation to YourCart

--- src/components/cart/your-cart.tsx
+++ src/components/cart/your-cart.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import {useRef, useState} from 'react'
 
 const YourCart = () => {
+    const [isValid, setIsValid]= useState(true)
+    const textref = useRef(null);
+
+    const onHandleKeyDown = ((e:React.KeyboardEvent<HTMLInputElement>)=> {
+        if (e.key === "Enter"){
+            setIsValid(false);
+        }
+      })
+    const onClickHandler = ()=> {
+        setIsValid(false);
+    }
+
   return (
-    <div>
-      <p>Your cart</p>
+    <div className=' my-4 bg-white p-5 rounded-xl'>
+      <p className='text-xl font-medium'>Your cart</p>
+      <div className='mt-8 flex gap-x-12'>
+        <input type="text" placeholder='Promo code' className='rounded-xl h-11 outline-none 
+        border pl-4 w-2/3' ref={textref} onKeyDown={onHandleKeyDown}/>
+        <button className='text-base font-medium text-red-400'
+            onClick={onClickHandler}>Apply</button>
+      </div>
+      {isValid || <p className='ml-4 text-sm my-2 text-red-400'>This is an invalid promo code</p>}
+      
     </div>
   )
 }
