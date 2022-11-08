fe96750620f2c44fac5c89cac681433a6d2fade1
Artur <helter@wp.pl>
2022-11-08 10:03:24 +0100

    create useChosenProducts hook to set and save data in Local Storage

---
+++ src/hooks/use-chosen-products.tsx
@@ -0,0 +1,23 @@
+import {useEffect, useState} from 'react'
+
+const getStorageValue = () => {
+    const saved = localStorage.getItem('products') as string
+    const initial = JSON.parse(saved);
+    return initial || []
+  }
+
+const useChosenProducts = () => {
+    const [products, setProducts] = useState(() => {
+        return getStorageValue();
+      });
+
+      useEffect(() => {
+        // storing input name
+        localStorage.setItem('products', JSON.stringify(products));
+      }, [products]);
+
+  return [products, setProducts]
+  
+}
+
+export default useChosenProducts
