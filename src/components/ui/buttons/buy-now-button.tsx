d4482c92caf08a25b61e3e6ab62c40e23b0b8306
Artur <helter@wp.pl>
2022-11-08 10:41:54 +0100

    add product to LocalStorage when BuyNowButton is clicked

--- src/components/ui/buttons/buy-now-button.tsx
+++ src/components/ui/buttons/buy-now-button.tsx
@@ -4,10 +4,23 @@ import useChosenProducts from "../../../hooks/use-chosen-products"
 const BuyNowButton = ({productID}:{productID:string}) => {
     const navigate = useNavigate()
     const [products, setProducts] = useChosenProducts()
-    const onClickHandler = () => {
-      console.log(products)
-      navigate('/cart')
+    const prodObj ={
+      id: productID,
+      quantity: 1,
+      checked: true
     }
+    const isProductAlreadyChosen = products?.some((item:any) => item.id === productID )
+    const onClickHandler = () => {
+      
+      if(isProductAlreadyChosen){
+        navigate('/cart')
+      } else{
+        setProducts((items:any)=> [prodObj, ...items])
+        navigate('/cart')
+      }
+      
+    }
+
     return (
       <button className='bg-red-500 opacity-80 text-white text-base font-medium
       py-3 px-5 rounded-xl transition-opacity hover:opacity-95 duration-500'
