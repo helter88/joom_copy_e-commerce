3e0b1140c61fadb9ace7cdd606360cc761510d8f
Artur <helter@wp.pl>
2022-11-09 12:48:25 +0100

    add functionality for Yes and No button in RemoveAllPopover

--- src/components/cart/cart-panel.tsx
+++ src/components/cart/cart-panel.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import {ReactComponent as Bin} from '../../assets/img/bin-icon.svg';
 import RemoveAllPopover from '../ui/remove-all-popover';
 
 const CartPanel = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen((stat)=> !stat)
     const onHandleRemoveAll = () => {
-        
+        toggle()
     }
   return (
     <div className='flex justify-between my-4
@@ -15,9 +18,9 @@ const CartPanel = () => {
               <label htmlFor="checked-checkbox" className="text-sm 
                 font-medium">Select all</label>  
             </div>
-           <RemoveAllPopover />
+           <RemoveAllPopover openStatus={isOpen} setStatus={toggle}/>
        </div>
-        <div className='flex gap-x-2' onClick={onHandleRemoveAll}>
+        <div className='flex gap-x-2 cursor-pointer' onClick={onHandleRemoveAll}>
             <Bin className='text-red-400'/>
             <p className="text-sm text-red-400">Remove all selected</p>  
         </div>
--- src/components/ui/remove-all-popover.tsx
+++ src/components/ui/remove-all-popover.tsx
@@ -1,15 +1,23 @@
 import useChosenProducts from '../../hooks/use-chosen-products'
 
-const RemoveAllPopover = () => {
+interface RemoveAllPopoverType {
+    openStatus: boolean,
+    setStatus: () => void
+}
+
+const RemoveAllPopover:React.FC<RemoveAllPopoverType> = ({openStatus, setStatus}) => {
     const [products, setProducts] = useChosenProducts()
     const onYesHandler= () => {
         setProducts([]);
+        setStatus();
+
     }
+    const activeStyle= openStatus ? 'scale-100' : 'scale-0'
 
   return (
-    <div className='absolute left-64 -top-8 bg-white border shadow p-3
+    <div className={`absolute left-64 -top-8 bg-white border shadow p-3
          shadow-slate-200 z-10 rounded-lg w-max 
-         flex flex-col items-center '>
+         flex flex-col items-center ${activeStyle} transition-all`}>
         <span className='text-sm'>Are you sure that you 
             want to remove 2 items from your cart?
         </span>
@@ -18,7 +26,7 @@ const RemoveAllPopover = () => {
              text-white rounded-full hover:opacity-80' onClick={onYesHandler}>Yes</button>
             <button className='text-sm py-0.5 px-10 border
              border-slate-400 text-slate-400 bg-white rounded-full'
-             onClick={() => console.log("no")}>No</button>  
+             onClick={setStatus}>No</button>  
         </div>
     </div>
   )
