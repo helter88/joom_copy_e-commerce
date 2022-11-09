c83e96cf6e5ccadd89edc810034aefb76594dca0
Artur <helter@wp.pl>
2022-11-09 11:17:30 +0100

    extract part of code from cart-with-product to cart-panel

---
+++ src/components/cart/cart-panel.tsx
@@ -0,0 +1,20 @@
+import {ReactComponent as Bin} from '../../assets/img/bin-icon.svg';
+
+const CartPanel = () => {
+  return (
+    <div className='flex justify-between my-4
+    bg-white p-5 rounded-xl'>
+      <div className='flex items-center gap-x-2'>
+           <input type="checkbox" className="border border-red-400 w-5 h-5 bg-white
+        accent-red-500 cursor-pointer " />
+           <label htmlFor="checked-checkbox" className="text-sm font-medium">Select all</label>
+       </div>
+       <div className='flex gap-x-2'>
+           <Bin className='text-red-400'/>
+           <p className="text-sm text-red-400">Remove all selected</p>  
+       </div>
+   </div>
+  )
+}
+
+export default CartPanel
--- src/components/cart/cart-with-products.tsx
+++ src/components/cart/cart-with-products.tsx
@@ -1,6 +1,7 @@
 import {ReactComponent as Bin} from '../../assets/img/bin-icon.svg';
 import useChosenProducts from '../../hooks/use-chosen-products';
 import { ChosenProductType } from '../ui/buttons/buy-now-button';
+import CartPanel from './cart-panel';
 import CartProduct from './cart-product';
 import YourCart from './your-cart';
 
@@ -17,21 +18,10 @@ const CartWithProducts = () => {
         <p className='text-2xl font-medium mt-8 mb-3'>My cart</p>
         <div className='grid grid-cols-3 gap-x-8'>
             <div className='col-span-2 '>
-                <div className='flex justify-between my-4
-                 bg-white p-5 rounded-xl'>
-                   <div className='flex items-center gap-x-2'>
-                        <input type="checkbox" className="border border-red-400 w-5 h-5 bg-white
-                     accent-red-500 cursor-pointer " />
-                        <label htmlFor="checked-checkbox" className="text-sm font-medium">Select all</label>
-                    </div>
-                    <div className='flex gap-x-2'>
-                        <Bin className='text-red-400'/>
-                        <p className="text-sm text-red-400">Remove all selected</p>  
-                    </div>
+              <CartPanel />
+              <div className=' my-4 bg-white p-5 rounded-xl'>
+                  {allCartProducts}
                 </div>
-                <div className=' my-4 bg-white p-5 rounded-xl'>
-                   {allCartProducts}
-                 </div>
             </div>
             <div className='col-span-1'>
                 <YourCart />
