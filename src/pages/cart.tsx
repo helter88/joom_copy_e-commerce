d660b8d18db89408b5480a0538ad6165a41d2c87
Artur <helter@wp.pl>
2022-11-12 19:06:59 +0100

    render CartWithProducts or CartEmpty in Cart depend on products added to cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,12 +1,17 @@
 import CartEmpty from "../components/cart/cart-empty"
 import CartWithProducts from "../components/cart/cart-with-products"
+import { useLocalStorage } from 'usehooks-ts';
+import { ChosenProductType } from "../components/ui/buttons/buy-now-button";
+
 
 
 const Cart = () => {
+  const [products, setProducts] = useLocalStorage<ChosenProductType[]|[]>('products',[]);
+  const isAnyProductAdded = products.length ===0 ? false : true
   return (
     <div className='mt-16 pb-24 bg-slate-100 absolute inset-x-0'>
-      <CartWithProducts />
-      {/* <CartEmpty /> */}
+      {isAnyProductAdded ? <CartWithProducts /> : <CartEmpty /> }
+      
     </div>
   )
 }
