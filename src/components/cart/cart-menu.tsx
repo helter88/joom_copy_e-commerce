5f28ff252961ba1b971dc4025010d4f2e906ae68
Artur <helter@wp.pl>
2022-11-14 16:28:02 +0100

    add functionality to Go to cart button in CartMenu

--- src/components/cart/cart-menu.tsx
+++ src/components/cart/cart-menu.tsx
@@ -2,7 +2,7 @@ import CartMenuProduct from "./cart-menu-product"
 import CartMenuTop from "./cart-menu-top"
 import { useLocalStorage } from "usehooks-ts";
 import { ChosenProductType } from "../ui/buttons/buy-now-button";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export interface CartMenuType {
   windowStatus: boolean;
@@ -13,6 +13,7 @@ export interface CartMenuType {
 const CartMenu:React.FC<CartMenuType> = ({windowStatus, handelMouseEnter, handleMouseLeave}) => {
   const [products, setProducts] = useLocalStorage<ChosenProductType[]|[]>('products',[]);
   const {pathname} = useLocation()
+  const navigate = useNavigate();
   
   const isHiddenStyle = () => {
     if (products?.length !== 0 && windowStatus && pathname === "/cart"){
@@ -31,7 +32,9 @@ const CartMenu:React.FC<CartMenuType> = ({windowStatus, handelMouseEnter, handle
       <CartMenuProduct key={product?.id} prodId={product?.id} />
   )
 
-
+  const onClickHandler = () => {
+      navigate('/cart')
+  }
  
   return (
     <div className={`absolute w-80 bg-white border shadow p-3
@@ -43,7 +46,7 @@ const CartMenu:React.FC<CartMenuType> = ({windowStatus, handelMouseEnter, handle
       </div>
       <div className="border-t pt-4 flex justify-center">
         <button className="bg-red-400 text-white rounded-lg w-full
-            hover:opacity-80 py-2"> Go to cart</button>
+            hover:opacity-80 py-2" onClick={onClickHandler}> Go to cart</button>
       </div>
       
     </div>
