7a378e1e88bdba767eab026dbe298b462d2e809f
Artur <helter@wp.pl>
2022-11-15 13:20:45 +0100

    add price sum to BuyForButton

--- src/components/ui/buttons/buy-for-button.tsx
+++ src/components/ui/buttons/buy-for-button.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocalStorage } from "usehooks-ts";
+import { getPrice } from '../../../utils/fetch-product-price';
+import { ChosenProductType } from './buy-now-button';
+
 
 const BuyForButton = () => {
+  const [products] = useLocalStorage<ChosenProductType[]|[]>('products',[]);
+  const [priceSum, setPriceSum] = useState(0);
+
+
+const productsCheckedList = products?.filter((prod:ChosenProductType) => {
+    return prod.checked === true
+})
+
+
+
+
+useEffect(() => {
+  const totalPrice = productsCheckedList?.reduce(async(prev:Promise<number>, current:ChosenProductType) => {
+    let preVal = await prev
+    const price = await getPrice(current.id);
+    return preVal + current.quantity*Number(price)
+  }, Promise.resolve(0)) ||0
+    totalPrice.then((sum)=> {
+      setPriceSum(sum)
+    })
+}, [products])
+
+
+
+
   return (
     <button className='w-full bg-red-400 text-white text-lg 
     py-3 rounded-xl hover:opacity-80 duration-300 my-5'>
-      Buy for PLN
+      Buy for PLN {priceSum}
     </button>
   )
 }
